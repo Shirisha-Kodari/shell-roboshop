78cab7c612e471e29b4162af6ae212ab0ae79ac6
SHIRISHA KODARI <shirishakodari35@gmail.com>
2026-06-01 20:38:51 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)  
 R="\e[31m" 
 Y="\e[33m" 
-N="\e[0m" 
+N="\e[0m"  
 LOGS_FOLDER="/var/log/roboshop-logs" 
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1) 
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
